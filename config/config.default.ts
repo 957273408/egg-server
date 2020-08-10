9722c663d0dec6b621a658e0f771a903cbe93237
v_sudsun <v_sudsun@tencent.com>
2020-08-10 14:43:24 +0800

    xx

--- config/config.default.ts
+++ config/config.default.ts
@@ -10,20 +10,7 @@ export default (appInfo: EggAppInfo) => {
             enable: false,
         }
     }
-    config.sequelize = {
-        // 单数据库信息配置
-        dialect: 'mysql',
-        // host
-        host: '39.107.89.227',
-        // 端口号
-        port: 3306,
-        // 用户名
-        username: 'sud',
-        // 密码
-        password: 'Sun123456.',
-        // 数据库名
-        database: 'egg-sequelize-doc-default',
-    };
+  
 
 
 
